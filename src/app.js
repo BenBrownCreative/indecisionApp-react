4710bb5c5d1beeb86d36b46d870d6e80f3503539
Ben Brown <designwithbrown@gmail.com>
2020-08-11 23:15:06 -0500

    added react components

--- src/app.js
+++ src/app.js
@@ -1,17 +1,68 @@
 // can pass in props to called components so they render differently 
 
 class IndecisionApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.handlePick = this.handlePick.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            options: [],
+            option: ''
+        };
+    }
+
+    handleDeleteOptions() {
+        this.setState(() => {
+            return {
+                options: [],
+                option: ''
+            }
+        })
+    }
+    handlePick() {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[randomNum];
+        this.setState(() => {
+            return {
+                option: option
+            }
+        })
+    }
+    handleAddOption(option) {
+        if (!option) {
+            return 'Enter Valid Value to add item'
+        }
+        else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists'
+        }
+        this.setState((prevState) => {
+            // dont want try and manipulate the current state object directly
+            // instead of doing push to the array we need to get the new array and return it
+            return {
+                options: prevState.options.concat([option])
+            }
+        })
+    }
     render() {
         const title = 'Indecision';
         const subtitle = 'Put your life into the hands of a computer';
-        let options = ['thing one', 'thing two', 'thing ben', 'thing three'];
 
         return (
             <div>
                 <Header title={title} subtitle={subtitle}/>
-                <Action />
-                <Options options={options} />
-                <AddOption />
+                <Action 
+                hasOptions={this.state.options.length > 0}
+                handlePick={this.handlePick}
+                pick={this.state.option}
+                />
+                <Options 
+                options={this.state.options} 
+                handleDeleteOptions={this.handleDeleteOptions}
+                />
+                <AddOption 
+                handleAddOption={this.handleAddOption}
+                />
             </div>
         )
     }
@@ -20,7 +71,6 @@ class IndecisionApp extends React.Component {
 // react component requires render
 class Header extends React.Component {
     render() {
-        console.log(this.props);
         return (
             <div>
                 <h1>{this.props.title}</h1>
@@ -31,27 +81,35 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
-    handlePick() {
-        alert('handle pick');
-    }
-
     render() {
         return (
             <div>
-                <button onClick={this.handlePick}>What should I do?</button>
+                <button 
+                onClick={this.props.handlePick}
+                disabled={!this.props.hasOptions}
+                >
+                What should I do?
+                </button>
+                {this.props.pick.length > 0 &&<p>{this.props.pick}</p>}
             </div>
         );
     }
 }
 
+// the this binding gets lost when defining methods inside the react component
+// the this binding is correct in the render and constructor function
+// in order to maintain the correct this binding we need to set the this binding for the function in the constructor
+
 class Options extends React.Component {
-    removeAll() {
-        alert('removing all options');
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    // }
+    
     render() {
         return (
             <div>
-                <button onClick={this.removeAll}>Remove All Options</button>
+                <button onClick={this.props.handleDeleteOptions}>Remove All Options</button>
                 {
                     this.props.options.map((option) => <Option key={option} optionText={option}/>)
                 }
@@ -73,22 +131,37 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOption = this.handleAddOption.bind(this);
+
+        this.state = {
+            error: undefined
+        }
+    }
     handleAddOption(e) {
         e.preventDefault();
         const option = e.target.elements.option.value.trim();
-
-        if (option) {
-            alert(option);
-        }
-
+        const error = this.props.handleAddOption(option);
+        // reset the input to empty
+        e.target.elements.option.value = '';
+        this.setState(() => {
+            return {
+                error // this is === error: error
+            }
+        })
     }
 
     render() {
         return (
-            <form onSubmit={this.handleAddOption}>
-                <input type='text' name='option' placeholder='add option'></input>
-                <button type='submit'>Submit</button>
-            </form>
+            <div>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.handleAddOption}>
+                    <input type='text' name='option' placeholder='add option'></input>
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
+            
         );
     }
 }
---
+++ src/playground/counter-example-refactor.js
@@ -0,0 +1,50 @@
+// this.setState is an asyncronous call, will not be immediately available
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleResest = this.handleResest.bind(this);
+
+        this.state = {
+            count: 0
+        }
+    }
+    handleAddOne() {
+        // in order to manipulate the state variable we need to call setState function
+        // the first argument is the previous state
+        this.setState((prevState) => {
+            // only need to provide the state properties you want to change
+            return {
+                count: prevState.count + 1
+            }
+        })
+    }
+    handleMinusOne() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1
+            }
+        })
+    }
+    handleResest() {
+        this.setState(() => {
+            return {
+                count: 0 
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleResest}>reset</button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'))
