19f3e597c55cc0e38b9c0606ae068f04222ad37c
Ben Brown <designwithbrown@gmail.com>
2020-09-24 10:47:09 -0500

    adding webpack and configuration

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// import built in node path
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+};
